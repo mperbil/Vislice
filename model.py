8d3c9bbab77046c50080d9ed468383bd59b8ff2c
unknown <mperbil@gmail.com>
2019-05-13 18:41:24 +0200

    zacetni model

--- model.py
+++ model.py
@@ -1,3 +1,5 @@
+import random
+
 STEVILO_DOVOLJENIH_NAPAK = 10
 PRAVILNA_CRKA = '+'
 PONOVLJENA_CRKA = '0'
@@ -6,7 +8,7 @@ ZMAGA = 'W'
 PORAZ = 'X'
 
 class Igra:
-    def __init__(self, geslo, crke):
+    def __init__(self, geslo, crke=[]):
         self.geslo = geslo
         self.crke = crke
     
@@ -34,9 +36,51 @@ class Igra:
         return True
     
     def poraz(self):
-        return self.napacne_crke >= STEVILO_DOVOLJENIH_NAPAK:
+        return self.napacne_crke() >= STEVILO_DOVOLJENIH_NAPAK:
 
-    def 
+    def pravilni_del_gesla(self):
+        sez = []
+        for crka in self.crke:
+            if crka in self.pravilne_crke():
+                sez.append(crka)
+            else:
+                sez.append('_')
+        return sez
+
+    def nepravilni_ugibi(self):
+        niz = ''
+        niz.join(self.napacne_crke())
+        return niz.split(' ')
+
+    def ugibaj(self, crka):
+        crka = crka.upper()
+        if crka in self.crke:
+            return PONOVLJENA_CRKA
+        else:
+            self.crke.append(crka)
+            if crka in self.pravilne_crke():
+                if self.zmaga():
+                    return ZMAGA
+                else:
+                    return PRAVILNA_CRKA
+            else:
+                if self.poraz():
+                    return PORAZ
+                else:
+                    return NAPACNA_CRKA
+
+bazen_besed=[]
+with open('besede.txt') as dat:
+    for vrstica in dat:
+        bazen_besed.append(vrstica.strip().upper())
+
+def nova_igra():
+    return Igra(random.choice(bazen_besed))
+
+
+        
+
+
         
 
 
