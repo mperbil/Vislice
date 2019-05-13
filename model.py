e5513a563ef43c1139ca9d07680937d12bb87161
unknown <mperbil@gmail.com>
2019-05-13 18:29:30 +0200

    metode

---
+++ model.py
@@ -0,0 +1,43 @@
+STEVILO_DOVOLJENIH_NAPAK = 10
+PRAVILNA_CRKA = '+'
+PONOVLJENA_CRKA = '0'
+NAPACNA_CRKA = '-'
+ZMAGA = 'W'
+PORAZ = 'X'
+
+class Igra:
+    def __init__(self, geslo, crke):
+        self.geslo = geslo
+        self.crke = crke
+    
+    def napacne_crke(self):
+        sez = []
+        for crka in self.crke:
+            if crka not in self.geslo:
+                sez.append(crka)
+        return sez
+
+    def pravilne_crke(self):
+        sez = []
+        for crka in self.crke:
+            if crka in self.geslo:
+                sez.append(crka)
+        return sez
+
+    def stevilo_napak(self):
+        return len(self.napacne_crke())
+
+    def zmaga(self):
+        for crka in self.geslo:
+            if crka not in self.crke:
+                return False
+        return True
+    
+    def poraz(self):
+        return self.napacne_crke >= STEVILO_DOVOLJENIH_NAPAK:
+
+    def 
+        
+
+
+
